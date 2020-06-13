650d225d655a9578d20d1c1d69e180050523e6b8
Rohit Iyer <rohitstar.iyer@gmail.com>
2020-06-13 18:17:34 +0530

    folders arranged

---
+++ Codes/Hotspot_identification.py
@@ -0,0 +1,40 @@
+import requests
+import json
+key="Your API Key"
+def get_data(containment_wards):
+    #address= ', '.join(map(str, containment_wards['Area Name'][i].split(',')[:2])) 
+    print(containment_wards['Sl no'])
+    response=""
+    address=containment_wards['Address']
+    address=address.upper()
+    address=address.replace(" ST,"," STREET,")
+    if(int(containment_wards['Ward'])==0):
+        URL=str("https://maps.googleapis.com/maps/api/geocode/json?address="+address+"&key="+key)
+        response=get_response(URL)
+    else:   
+        URL=str("https://maps.googleapis.com/maps/api/geocode/json?address="+address+", Ward "+str(int(containment_wards['Ward']))+", Kolkata"+"&key="+key)
+        response=get_response(URL)
+    received_data = json.loads(response.text)
+    if(len(received_data['results'])==0):
+        URL=str("https://maps.googleapis.com/maps/api/geocode/json?address="+str(containment_wards['Local area'])+", Kolkata"+"&key="+key)
+        response=get_response(URL)
+        received_data = json.loads(response.text)
+    try:
+        print("Success")
+        return ([received_data['results'][0]['geometry']['location']['lat'],received_data['results'][0]['geometry']['location']['lng'],containment_wards['Address']+" Ward: "+str(int(containment_wards['Ward']))])
+    except:
+        print(received_data)
+    return ""
+def get_response(URL):
+    response=""
+    tries=10
+    for i in range(tries):
+        try:
+            response=requests.get(URL)
+        except:
+            if(i<tries):
+                continue
+        break
+    if(response==""):
+        print("FAILED")
+    return response
