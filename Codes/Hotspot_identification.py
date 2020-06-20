49da81ade61aba1684503619383ac3d61fc69c60
Rohit Iyer <rohitstar.iyer@gmail.com>
2020-06-20 12:12:36 +0530

    Minor fixes

--- Codes/Hotspot_identification.py
+++ Codes/Hotspot_identification.py
@@ -1,21 +1,23 @@
 import requests
 import json
-key="Your API Key"
+from dotenv import load_dotenv 
+key = os.environ.get('key')
 def get_data(containment_wards):
     #address= ', '.join(map(str, containment_wards['Area Name'][i].split(',')[:2])) 
     print(containment_wards['Sl no'])
     response=""
+    received_data=""
     address=containment_wards['Address']
     address=address.upper()
     address=address.replace(" ST,"," STREET,")
     if(int(containment_wards['Ward'])==0):
         URL=str("https://maps.googleapis.com/maps/api/geocode/json?address="+address+"&key="+key)
         response=get_response(URL)
-    else:   
+    elif(len(address)>0):   
         URL=str("https://maps.googleapis.com/maps/api/geocode/json?address="+address+", Ward "+str(int(containment_wards['Ward']))+", Kolkata"+"&key="+key)
         response=get_response(URL)
-    received_data = json.loads(response.text)
-    if(len(received_data['results'])==0):
+        received_data = json.loads(response.text)
+    if(len(received_data)>0 and len(received_data['results'])==0 and len(str(containment_wards['Local area']))>0):
         URL=str("https://maps.googleapis.com/maps/api/geocode/json?address="+str(containment_wards['Local area'])+", Kolkata"+"&key="+key)
         response=get_response(URL)
         received_data = json.loads(response.text)
